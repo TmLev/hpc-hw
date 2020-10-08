591912c3b453a95bd8a268e87ddac8859064dc6d
TmLev <leva.bugatti@gmail.com>
2020-10-08 22:56:07 +0300

    feat: add state, extract bound

--- 1-knapsack/context.cpp
+++ 1-knapsack/context.cpp
@@ -13,3 +13,54 @@ auto MaxPrice::Update(int price) -> void {
 auto MaxPrice::Clear() -> void {
   price_.store(0);
 }
+
+auto Split(States states, std::size_t batch_size) -> std::vector<States> {
+  if (states.empty()) {
+    return {};
+  }
+
+  auto count = std::max(1ul, states.size() / batch_size);
+  auto splits = std::vector<States>{count};
+
+  for (auto i = std::size_t{0}; ; ++i) {
+    if (i + 1 == count) {
+      splits[i] = std::move(states);
+      break;
+    }
+
+    for (auto j = std::size_t{0}; j < batch_size; ++j) {
+      auto s = states.top();
+      states.pop();
+      splits[i].push(s);
+    }
+  }
+
+  return splits;
+}
+
+auto State::ComputeBound(const knapsack::Knapsack& ks) -> double {
+  if (current_weight > ks.capacity) {
+    return 0;
+  }
+
+  auto cs = cursor;
+  auto weight = current_weight;
+
+  bound = static_cast<double>(current_price);
+
+  while (++cs < ks.items.size() &&
+         weight + ks.items[cs].weight <= ks.capacity) {
+    weight += ks.items[cs].weight;
+    bound += ks.items[cs].price;
+  }
+
+  if (cs < ks.items.size()) {
+    bound += (ks.capacity - weight) * ks.items[cs].GetRank();
+  }
+
+  return bound;
+}
+
+auto operator<(const State& left, const State& right) -> bool {
+  return left.bound < right.bound;
+}
--- 1-knapsack/context.hpp
+++ 1-knapsack/context.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <atomic>
+#include <deque>
+#include <queue>
+#include <vector>
 
 #include <await/executors/thread_pool.hpp>
 
@@ -16,15 +19,33 @@ struct MaxPrice {
   std::atomic<int> price_{0};
 };
 
-struct Context {
-  // executor / thread pool
-  await::executors::IThreadPoolPtr tp;
+struct State {
+ public:
+  auto ComputeBound(const knapsack::Knapsack& ks) -> double;
 
-  // knapsack and current item
-  const knapsack::Knapsack& knapsack;
+ public:
+  // current item in knapsack
   std::size_t cursor{0};
 
   // accumulated price and weight for current set of included items
   int current_price{0};
   int current_weight{0};
+
+  // possible bound
+  double bound{0};
+};
+
+auto operator<(const State& left, const State& right) -> bool;
+
+using States = std::priority_queue<State>;
+
+auto Split(States states, std::size_t batch_size) -> std::vector<States>;
+
+struct Context {
+  // executor / thread pool
+  await::executors::IThreadPoolPtr tp;
+
+  // knapsack and set of states
+  const knapsack::Knapsack& knapsack;
+  States states{};
 };
