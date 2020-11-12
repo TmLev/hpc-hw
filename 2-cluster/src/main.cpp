e82686b8eb3a98beb23b955915a68922fb58ad19
TmLev <leva.bugatti@gmail.com>
2020-11-12 14:41:15 +0300

    feat: add main

--- 2-cluster/src/main.cpp
+++ 2-cluster/src/main.cpp
@@ -1,6 +1,41 @@
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
-#include <omp.h>
+
+#include "config.hpp"
+#include "solver.hpp"
+
+// NOLINTNEXTLINE
+using namespace config;
+
+auto RunTests(std::ostream& log = std::cerr) -> Durations {
+  auto durations = Durations{};
+
+  for (auto test = 1; test <= kTestCount; ++test) {
+    auto solver = Solver{kThreadCount};
+    auto input = "data/" + std::to_string(test);
+    auto output = "results/" + std::to_string(test);
+
+    auto start = Clock::now();
+    solver.Solve(input, output);
+    auto dur = std::chrono::duration_cast<Mcs>(Clock::now() - start).count();
+    durations.push_back(dur);
+
+    log << "Test " << test << " finished" << '\n';
+  }
+
+  log << std::flush;
+
+  return durations;
+}
 
 auto main() -> int {
-  std::cout << "Procs: " << omp_get_num_procs() << '\n';
+  std::srand(11);
+
+  auto durations = RunTests();
+
+  for (auto dur : durations) {
+    std::cout << dur << ", ";
+  }
+  std::cout << std::endl;
 }
