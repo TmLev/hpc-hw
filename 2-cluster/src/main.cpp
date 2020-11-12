800efe70158a8214567e17fbf69a86c091901e69
TmLev <leva.bugatti@gmail.com>
2020-11-12 17:08:01 +0300

    refactor: better test runner

--- 2-cluster/src/main.cpp
+++ 2-cluster/src/main.cpp
@@ -8,9 +8,7 @@
 // NOLINTNEXTLINE
 using namespace config;
 
-auto RunTests(std::ostream& log = std::cerr) -> Durations {
-  auto durations = Durations{};
-
+auto RunTests(std::ostream& log = std::cerr) -> void {
   for (auto test = 1; test <= kTestCount; ++test) {
     auto solver = Solver{};
     auto input = "data/" + std::to_string(test);
@@ -19,23 +17,14 @@ auto RunTests(std::ostream& log = std::cerr) -> Durations {
     auto start = Clock::now();
     solver.Solve(input, output);
     auto dur = std::chrono::duration_cast<Mcs>(Clock::now() - start).count();
-    durations.push_back(dur);
 
-    log << "Test " << test << " finished" << '\n';
+    log << dur << (test == kTestCount ? "" : ", ");
   }
 
   log << std::flush;
-
-  return durations;
 }
 
 auto main() -> int {
   std::srand(11);
-
-  auto durations = RunTests();
-
-  for (auto dur : durations) {
-    std::cout << dur << ", ";
-  }
-  std::cout << std::endl;
+  RunTests();
 }
