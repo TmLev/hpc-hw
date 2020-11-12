2c005c00ef8f64a2242b53e81c7062caa006eb14
TmLev <leva.bugatti@gmail.com>
2020-11-12 14:40:08 +0300

    feat: better config

--- 2-cluster/src/config.hpp
+++ 2-cluster/src/config.hpp
@@ -5,12 +5,23 @@
 
 namespace config {
 
+using Size = std::size_t;
+using Index = std::size_t;
+
+using Clock = std::chrono::steady_clock;
+using Mcs = std::chrono::microseconds;
+using Durations = std::vector<std::result_of_t<decltype (&Mcs::count)(Mcs)>>;
+
+constexpr auto kTestCount = 5;
+constexpr auto kThreadCount = 1;
+
 #if __cpp_lib_hardware_interference_size > 201703L
 
 using std::hardware_constructive_interference_size;
 using std::hardware_destructive_interference_size;
 
 #else
+
 // Lucky guess │ __cacheline_aligned │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ ...
 
 // NOLINTNEXTLINE
@@ -20,6 +31,7 @@ constexpr std::size_t hardware_constructive_interference_size =
 // NOLINTNEXTLINE
 constexpr std::size_t hardware_destructive_interference_size =
     2 * sizeof(std::max_align_t);
+
 #endif
 
 }  // namespace config
