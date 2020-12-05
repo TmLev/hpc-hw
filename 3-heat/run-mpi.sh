fd2d1e7a8182d624ee27f8956ab8013ea127ebbc
TmLev <leva.bugatti@gmail.com>
2020-12-05 23:18:06 +0300

    fix: add hack for avoiding mpi internal error

--- 3-heat/run-mpi.sh
+++ 3-heat/run-mpi.sh
@@ -5,4 +5,4 @@ PROCESSES=$1
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
 BIN_PATH="$ROOT/cmake-build-debug/3-heat/src/3-heat"
 
-mpirun --oversubscribe -np "$PROCESSES" "$BIN_PATH"
+TMPDIR=/tmp mpirun --oversubscribe -np "$PROCESSES" "$BIN_PATH"
