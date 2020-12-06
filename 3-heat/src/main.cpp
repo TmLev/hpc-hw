6f52fcd1c802addb7e836c3c8712ff86a8828a38
TmLev <leva.bugatti@gmail.com>
2020-12-06 19:31:24 +0300

    refactor: bump main

--- 3-heat/src/main.cpp
+++ 3-heat/src/main.cpp
@@ -1,77 +1,44 @@
 #include <iostream>
-#include <vector>
+#include <sstream>
 
 #include <mpi.h>
 
-////////////////////////////////////////////////////////////////////////////////
-
-constexpr auto kPieces = std::size_t{11};
-constexpr auto kLength = double{1};
-constexpr auto kThermalDiffusivity = double{1};  // k
-constexpr auto kSpaceStep = double{0.02};        // h
-constexpr auto kTimeStep = double{0.0002};       // dt
+#include "world-guard.hpp"
+#include "process.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define EXPECT_OK(call)                                             \
-  do {                                                              \
-    const auto ec = call;                                           \
-    if (ec != 0) {                                                  \
-      MPI_Abort(MPI_COMM_WORLD, ec);                                \
-      const auto ec_str = std::to_string(ec);                       \
-      throw std::runtime_error("MPI call failed: " #call + ec_str); \
-    }                                                               \
-  } while (false)
+auto ToString(const Process::DVec& heat, std::string_view delim = " ")
+    -> std::string {
+  auto ss = std::stringstream{};
+  auto first = true;
 
-// Log variable to any std::ostream as `name = value`.
-// Example:
-// ```cpp
-// auto x = 2;
-// auto ss = std::stringstream{};
-// ss << LOG(x);
-// assert(ss.str() == "x = 2");
-// ```
-#define LOG(variable) #variable " = " << variable
+  for (const auto& h : heat) {
+    if (!first) {
+      ss << delim;
+    } else {
+      first = false;
+    }
 
-////////////////////////////////////////////////////////////////////////////////
-
-struct WorldGuard {
-  WorldGuard(int c, char** v) : argc(c), argv(v) {
-    std::cout << "Starting...\n";
-    EXPECT_OK(MPI_Init(&argc, &argv));
+    ss << h;
   }
 
-  ~WorldGuard() {
-    MPI_Finalize();
-    std::cout << "Finished\n";
-  }
-
-  int argc;
-  char** argv;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
-auto GetRank() -> std::size_t {
-  auto rank = 0;
-  EXPECT_OK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-  assert(rank >= 0);
-  return static_cast<std::size_t>(rank);
-}
-
-auto GetProcessCount() -> std::size_t {
-  auto process_count = 0;
-  EXPECT_OK(MPI_Comm_size(MPI_COMM_WORLD, &process_count));
-  assert(process_count > 0);
-  return static_cast<std::size_t>(process_count);
+  return ss.str();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 auto main(int argc, char** argv) -> int {
-  auto guard = WorldGuard{argc, argv};
+  const auto guard = WorldGuard{argc, argv};
 
-  auto rank = GetRank();
-  auto process_count = GetProcessCount();
-  auto rod = std::vector<double>(kPieces + 2, 0);
+  auto process = Process();
+
+  const auto begin = MPI_Wtime();
+  const auto heat = process.ComputeHeat();
+  const auto dur = MPI_Wtime() - begin;
+
+  if (heat) {
+    std::cout << "Finished in " << dur << " seconds" << '\n';
+    std::cout << ToString(*heat) << '\n';
+  }
 }
