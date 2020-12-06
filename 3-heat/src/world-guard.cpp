1796a4fb0ec2bd853eff43046c173dacd98aabdb
TmLev <leva.bugatti@gmail.com>
2020-12-06 19:46:39 +0300

    fix: throw on failed condition

--- 3-heat/src/world-guard.cpp
+++ 3-heat/src/world-guard.cpp
@@ -3,13 +3,23 @@
 
 #include <mpi.h>
 
+#include "config.hpp"
 #include "macros.hpp"
 #include "world-guard.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
 
+using namespace config;  // NOLINT
+
+////////////////////////////////////////////////////////////////////////////////
+
 WorldGuard::WorldGuard(int c, char** v, std::ostream& l)
     : argc(c), argv(v), log(l) {
+  // NOLINTNEXTLINE
+  if constexpr (kTimeStep >= kSpaceStep * kSpaceStep / kThermalDiffusivity) {
+    throw std::runtime_error("Failed condition");
+  }
+
   log << "[T " << std::this_thread::get_id() << "] started\n";
   EXPECT_OK(MPI_Init(&argc, &argv));
 }
