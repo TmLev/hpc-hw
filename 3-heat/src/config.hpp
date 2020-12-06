d22a6086e9cbf335b28265656a7ac6b0ec0d141c
TmLev <leva.bugatti@gmail.com>
2020-12-06 19:30:39 +0300

    feat: extract config

---
+++ 3-heat/src/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace config {
+
+constexpr auto kLeftEnd = double{0};
+constexpr auto kRightEnd = double{1};
+constexpr auto kLength = kRightEnd - kLeftEnd;
+constexpr auto kPieces = std::size_t{2'000};
+constexpr auto kSpaceStep = kLength / (kPieces - 1);  // h
+
+constexpr auto kEnvHeat = double{0};             // environment temperature
+constexpr auto kInitHeat = double{1};            // u_0
+constexpr auto kThermalDiffusivity = double{1};  // k
+
+constexpr auto kTimeStep = double{0.0002};  // dt
+constexpr auto kTimeBegin = double{0};      // t_0
+constexpr auto kTimeEnd = double{0.1};      // T
+
+}  // namespace config
